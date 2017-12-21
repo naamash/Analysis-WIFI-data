201e8eded27c8b2ef996869225a08eea4f824b21
hadaral <hadaral1994@gmail.com>
2017-12-21 12:09:33 +0200

    remove import in Location

--- code/mainClass/main.java
+++ code/mainClass/main.java
@@ -38,10 +38,10 @@ public class main {
 //		File folder2=new File(foldername2);
 //
 //		
-//		String locationAlgo1 = "C:\\Users\\נעמה שטאובר\\Desktop\\Answer exampels\\Answer_Of_Matala_two_algo1.csv";
+//		String locationAlgo1 = "C:\\Users\\hadar\\Desktop\\Answer exampels\\matala2algo1.csv";
 //		FindLocation.Matala2_Algo1 (folder1, locationAlgo1);
 ////		
-//		String locationAlgo2 = "C:\\Users\\נעמה שטאובר\\Desktop\\Answer exampels\\Answer_Of_Matala_two_algo2.csv";
+//		String locationAlgo2 = "C:\\Users\\hadar\\Desktop\\Answer exampels\\matala2algo2.csv";
 //		FindLocation.Matala2_Algo2 (folder1,folder2,locationAlgo2);
 //		
 	    // } end of Matala two
